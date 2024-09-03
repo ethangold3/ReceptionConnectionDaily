f4c1a2562c04270719ba15bab2bd1b05f956da73
Ethan Goldberg <eag62@duke.edu>
2024-09-03 20:16:44 +0100

    Adjust Talisman CSP settings

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,35 @@ template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'te
 static_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'static'))
 
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
-Talisman(app, force_https=True)
+
+csp = {
+    'default-src': [
+        "'self'",
+        'https://code.jquery.com',
+        'https://cdnjs.cloudflare.com',
+        'https://fonts.googleapis.com',
+        'https://fonts.gstatic.com',
+    ],
+    'script-src': [
+        "'self'",
+        'https://code.jquery.com',
+        "'unsafe-inline'",
+        "'unsafe-eval'",
+    ],
+    'style-src': [
+        "'self'",
+        'https://code.jquery.com',
+        'https://cdnjs.cloudflare.com',
+        'https://fonts.googleapis.com',
+        "'unsafe-inline'",
+    ],
+    'font-src': [
+        "'self'",
+        'https://fonts.gstatic.com',
+    ],
+    'img-src': ["'self'", 'data:'],
+}
+
+Talisman(app, force_https=True, content_security_policy=csp)
 
 from app import routes
